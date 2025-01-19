47c6953360697fd22b4c7e138fa75a709a745641
shrimo <victor.lavrentev@gmail.com>
2025-01-20 01:51:23 +0200

    Added open3d

--- source/run.py
+++ source/run.py
@@ -1,46 +1,66 @@
+#!/usr/bin/python3.10
+
 import cv2
-import plyfile
 import numpy as np
-import matplotlib.pyplot as plt
 
 from kitti_reader import DatasetReaderKITTI
 from feature_tracking import FeatureTracker
-from utils import drawFrameFeatures, updateTrajectoryDrawing, savePly
+from utils import drawFrameFeatures
+from display_open3d import DisplayOpen3D
 
 if __name__ == "__main__":
     tracker = FeatureTracker()
     detector = cv2.GFTTDetector_create()
-    dataset_reader = DatasetReaderKITTI("../videos/KITTI/sequences/00/")
+    dataset_reader = DatasetReaderKITTI("./video/05/")
 
     K = dataset_reader.readCameraMatrix()
+    print(K)
 
     prev_points = np.empty(0)
     prev_frame_BGR = dataset_reader.readFrame(0)
     kitti_positions, track_positions = [], []
-    camera_rot, camera_pos = np.eye(3), np.zeros((3,1))
+    camera_rot, camera_pos = np.eye(3), np.zeros((3, 1))
+    cv2.namedWindow("2D Viewer", cv2.WINDOW_GUI_NORMAL | cv2.WINDOW_AUTOSIZE)
+    cv2.moveWindow("2D Viewer", 100, 100)
 
-    plt.show()
+    d3d = DisplayOpen3D(
+        width=1280,
+        height=720,
+        scale=0.05,
+        point_size=2.0,
+        write_pcd=False,
+    )
 
     # Process next frames
-    for frame_no in range(1, 250):
+    for frame_no in range(1, 2500):
         curr_frame_BGR = dataset_reader.readFrame(frame_no)
         prev_frame = cv2.cvtColor(prev_frame_BGR, cv2.COLOR_BGR2GRAY)
         curr_frame = cv2.cvtColor(curr_frame_BGR, cv2.COLOR_BGR2GRAY)
 
         # Feature detection & filtering
         prev_points = detector.detect(prev_frame)
-        prev_points = cv2.KeyPoint_convert(sorted(prev_points, key = lambda p: p.response, reverse=True))
-    
+        prev_points = cv2.KeyPoint_convert(
+            sorted(prev_points, key=lambda p: p.response, reverse=True)
+        )
+
         # Feature tracking (optical flow)
-        prev_points, curr_points = tracker.trackFeatures(prev_frame, curr_frame, prev_points, removeOutliers=True)
-        print (f"{len(curr_points)} features left after feature tracking.")
+        prev_points, curr_points = tracker.trackFeatures(
+            prev_frame, curr_frame, prev_points, removeOutliers=True
+        )
+        # print (f"{len(curr_points)} features left after feature tracking.")
 
         # Essential matrix, pose estimation
-        E, mask = cv2.findEssentialMat(curr_points, prev_points, K, cv2.RANSAC, 0.99, 1.0, None)
-        prev_points = np.array([pt for (idx, pt) in enumerate(prev_points) if mask[idx] == 1])
-        curr_points = np.array([pt for (idx, pt) in enumerate(curr_points) if mask[idx] == 1])
+        E, mask = cv2.findEssentialMat(
+            curr_points, prev_points, K, cv2.RANSAC, 0.99, 1.0, None
+        )
+        prev_points = np.array(
+            [pt for (idx, pt) in enumerate(prev_points) if mask[idx] == 1]
+        )
+        curr_points = np.array(
+            [pt for (idx, pt) in enumerate(curr_points) if mask[idx] == 1]
+        )
         _, R, T, _ = cv2.recoverPose(E, curr_points, prev_points, K)
-        print(f"{len(curr_points)} features left after pose estimation.")
+        # print(f"{len(curr_points)} features left after pose estimation.")
 
         # Read groundtruth translation T and absolute scale for computing trajectory
         kitti_pos, kitti_scale = dataset_reader.readGroundtuthPosition(frame_no)
@@ -50,15 +70,27 @@ if __name__ == "__main__":
         camera_pos = camera_pos + kitti_scale * camera_rot.dot(T)
         camera_rot = R.dot(camera_rot)
 
-        kitti_positions.append(kitti_pos)
-        track_positions.append(camera_pos)
-        updateTrajectoryDrawing(np.array(track_positions), np.array(kitti_positions))
-        drawFrameFeatures(curr_frame, prev_points, curr_points, frame_no)
+        track_positions.append(
+            np.array([camera_pos[0][0], 0.0, camera_pos[2][0]]) * 0.05
+        )
+        kitti_positions.append(
+            np.array([kitti_pos[0], kitti_pos[1], kitti_pos[2]]) * 0.05
+        )
+        cam_colors = [(1.0, 0.0, 0.0)] * len(track_positions)
+        kitti_colors = [(0.0, 1.0, 0.0)] * len(kitti_positions)
 
-        if cv2.waitKey(1) == ord('q'):
+        drawFrameFeatures(curr_frame, prev_points, curr_points, frame_no)
+        d3d.send_to_visualization(
+            np.array(track_positions),
+            np.array(kitti_positions),
+            cam_colors,
+            kitti_colors,
+        )
+        # print('03')
+
+        if cv2.waitKey(1) == ord("q"):
             break
-            
+
         prev_points, prev_frame_BGR = curr_points, curr_frame_BGR
 
     cv2.destroyAllWindows()
-
--- source/utils.py
+++ source/utils.py
@@ -1,35 +1,38 @@
 import cv2
-import matplotlib.pyplot as plt
+import numpy as np
+
 
 # Draws detected and tracked features on a frame (motion vector is drawn as a line).
-# @param frame Frame to be used for drawing (will be converted to RGB).
-# @param prevPts Previous frame keypoints.
-# @param currPts Next frame keypoints.
 def drawFrameFeatures(frame, prevPts, currPts, frameIdx):
     currFrameRGB = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
-    for i in range(len(currPts)-1):
-        cv2.circle(currFrameRGB, tuple(currPts[i]), radius=3, color=(200, 100, 0))
-        cv2.line(currFrameRGB, tuple(prevPts[i]), tuple(currPts[i]), color=(200, 100, 0))
-        cv2.putText(currFrameRGB, "Frame: {}".format(frameIdx), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200))
-        cv2.putText(currFrameRGB, "Features: {}".format(len(currPts)), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200))    
-    cv2.imshow("Frame with keypoints", currFrameRGB)
-
-#
-# @param trackedPoints
-# @param groundtruthPoints
-def updateTrajectoryDrawing(trackedPoints, groundtruthPoints):
-    plt.cla()
-    plt.plot(trackedPoints[:,0], trackedPoints[:,2], c='blue', label="Tracking")
-    plt.plot(groundtruthPoints[:,0], groundtruthPoints[:,2], c='green', label="Ground truth")
-    plt.title("Trajectory")
-    plt.legend()
-    plt.draw()
-    plt.pause(0.01)
-
-def savePly(points, colors, output_file):
-    vertexes = [ (p[0], p[1], p[2], c[0], c[1], c[2]) for p, c in zip(points, colors)]
-    vertexes = [ v for v in vertexes if v[2] >= 0 ] # Discard negative z
-    dtypes = [('x', 'f8'), ('y', 'f8'), ('z', 'f8'), ('red', 'u1'), ('green', 'u1'), ('blue', 'u1')]
-    array = np.array(vertexes, dtype=dtypes)
-    element = plyfile.PlyElement.describe(array, "vertex")
-    plyfile.PlyData([element]).write(output_file)
+    for i in range(len(currPts) - 1):
+        # print(currPts[i])
+        cv2.circle(
+            currFrameRGB,
+            (np.int32(currPts[i][0]), np.int32(currPts[i][1])),
+            radius=3,
+            color=(0, 255, 0),
+        )
+        cv2.line(
+            currFrameRGB,
+            (np.int32(currPts[i][0]), np.int32(currPts[i][1])),
+            (np.int32(prevPts[i][0]), np.int32(prevPts[i][1])),
+            color=(0, 0, 255),
+        )
+        cv2.putText(
+            currFrameRGB,
+            "Frame: {}".format(frameIdx),
+            (10, 20),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (0, 250, 250),
+        )
+        cv2.putText(
+            currFrameRGB,
+            "Features: {}".format(len(currPts)),
+            (10, 40),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (0, 250, 250),
+        )
+    cv2.imshow("2D Viewer", currFrameRGB)
